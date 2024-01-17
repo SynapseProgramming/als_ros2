dee35a7e0413dec7cefa519e8a26e3916207ec87
Roald Ong <roald.ong@gmail.com>
2024-01-17 15:53:52 +0800

    Ported over mcl class variables

---
+++ src/mcl.cpp
@@ -0,0 +1,9 @@
+#include <als_ros2/MCL.h>
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    //   rclcpp::spin(std::make_shared<als_ros2::MCL>());
+    rclcpp::shutdown();
+    return 0;
+}
